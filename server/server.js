e98e320d901d719a21b0d7a7c543a38d264351fd
Na Wei <na.wei95@hotmail.com>
2021-04-07 09:42:55 +0200

    task 2

--- server/server.js
+++ server/server.js
@@ -2,20 +2,23 @@ const express = require('express');
 const http = require('http');
 const bodyParser = require('body-parser');
 const app = express();
-const cors = require('cors')
+const cors = require('cors');
 const products = require('./db');
+const productsAPI = require('./api/products');
+const productAPI = require('./api/product');
 
-app.use(cors())
+app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+app.use('/api/products', productsAPI);
 
 http.createServer(app).listen(3001, () => {
   console.log('Listen on 0.0.0.0:3001');
 });
 
-app.get('/', (_, res) => {
-  res.send({ status: 200 });
-});
+// app.get('/', (_, res) => {
+//   res.send({ status: 200 });
+// });
 
 process.on('SIGINT', function () {
   process.exit();
