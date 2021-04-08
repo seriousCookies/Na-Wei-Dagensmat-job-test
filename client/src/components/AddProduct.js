1b8232f13d2951b5746ec5f61aee77c74745b1c6
Na Wei <na.wei95@hotmail.com>
2021-04-08 09:13:55 +0200

    toast added

--- client/src/components/AddProduct.js
+++ client/src/components/AddProduct.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Container, Form, Button } from 'react-bootstrap';
+import { Container, Form, Button, Toast } from 'react-bootstrap';
 import axios from 'axios';
 const URL = 'http://localhost:3001/api/create';
 const categoryOptions = ['greens', 'meat', 'fish'];
@@ -11,18 +11,18 @@ const initialState = {
 
 const AddProduct = () => {
   const [newProduct, setNewProduct] = useState(initialState);
+  const [showToast, setShowToast] = useState(false);
   const handleChanges = (e) => {
     e.preventDefault();
     const key = e.target.id;
     const value = e.target.value;
     setNewProduct({ ...newProduct, [key]: value });
   };
-
   const formHandler = (e) => {
     e.preventDefault();
     axios
       .post(URL, newProduct)
-      .then(() => console.log('product Added', newProduct))
+      .then(() => setShowToast(true))
       .catch((e) => console.log(e));
     e.target.reset();
   };
@@ -30,6 +30,21 @@ const AddProduct = () => {
     <Container>
       <Form.Text as="h1">Add New Product</Form.Text>
       <Form onSubmit={formHandler}>
+        <Toast
+          onClose={() => setShowToast(false)}
+          show={showToast}
+          delay={2000}
+          autohide
+          style={{
+            position: 'absolute',
+            top: 0,
+            right: 0,
+          }}
+        >
+          <Toast.Body>
+            <strong className="mr-auto">New product added!</strong>
+          </Toast.Body>
+        </Toast>
         <Form.Group controlId="name">
           <Form.Label>Product Name</Form.Label>
           <Form.Control
